2604880f701ce112ab40a87e0d7e0f7c48b7d0d8
Felipe Diaz <pipexir@gmail.com>
2017-12-05 14:03:41 -0500

    Creating node project

--- weather-query.js
+++ weather-query.js
@@ -1,3 +1,56 @@
+
+
 /**
  * Created by USER on 5/12/2017.
  */
+
+(function(){
+
+
+
+
+    var  WeatherService = require('./services/WeatherService');
+
+    var configuration = {
+        city: 'Miami',
+        APIKEY: null,
+        format: 'json',
+        parameters: null
+    };
+
+
+    exports.setApiKey = function (APIKEY) {
+      configuration.APIKEY = APIKEY;
+    };
+    exports.getApiKey = function () {
+        return configuration.APIKEY;
+    };
+
+    exports.getConfiguration = function () {
+        return configuration
+    };
+
+    exports.setCityName = function(cityName){
+        configuration.parameters = null;
+        configuration.parameters = {city: cityName};
+    };
+
+    exports.setCoordinates = function (coordinates) {
+        configuration.parameters = null;
+        configuration.parameters = {coordinates: coordinates};
+    };
+
+
+    exports.weather = function (callback) {
+
+        WeatherService.getWeather(configuration, callback);
+    };
+
+
+
+
+})();
+
+
+
+
